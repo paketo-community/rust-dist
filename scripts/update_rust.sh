49c4ce2bb197051f2247d452212875ca47f34e57
Daniel Mikusa <dan@mikusa.com>
2020-07-05 14:07:18 -0400

    Added helper tool for generating buildpack dependency metadata on updates.

---
+++ scripts/update_rust.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+if [ -z "$1" ]; then
+    echo
+    echo "** A version is required"
+    echo
+    echo "./update_rust.sh <version>"
+    echo
+    exit -1
+fi
+
+mkdir -p .rust-bins
+cd .rust-bins/
+
+# Download
+if [ ! -f "rust-$1-x86_64-unknown-linux-gnu.tar.gz" ]; then
+    curl -L -O "https://static.rust-lang.org/dist/rust-$1-x86_64-unknown-linux-gnu.tar.gz"
+fi
+
+if [ ! -f "rustc-$1-src.tar.gz" ]; then
+    curl -L -O "https://static.rust-lang.org/dist/rustc-$1-src.tar.gz"
+fi
+
+# Calculate Hashes
+BIN_HASH=$(shasum -a 256 "rust-$1-x86_64-unknown-linux-gnu.tar.gz")
+SRC_HASH=$(shasum -a 256 "rustc-$1-src.tar.gz")
+
+cat <<EOF
+  [[metadata.dependencies]]
+    id = "rust"
+    name = "Rust Standalone Installer"
+    version = "$1"
+    uri = "https://static.rust-lang.org/dist/rust-$1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 = "$BIN_HASH"
+    source = "https://static.rust-lang.org/dist/rustc-$1-src.tar.gz"
+    source_256 = "$SRC_HASH"
+    stacks = ["io.paketo.stacks.tiny", "io.buildpacks.stacks.bionic", "org.cloudfoundry.stacks.cflinuxfs3"]
+EOF
