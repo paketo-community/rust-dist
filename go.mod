6f056c7d9005020be44987de3b70e236fed40cee
Daniel Mikusa <dmikusa@vmware.com>
2021-09-08 15:45:11 -0400

    Switch to using libcnb & libpak. (#101)
    - This is done because it handles build plan entries in a way that works with rustup buildpack
      - It adds instant support for a BOM with details about the installed package
      - It enables support for buildpack API 0.6
      - It adds license info as metadata
      - It switches to libraries that I'm more familiar with using as a buildpacks author
    
    In addition:
    
    1. Includes some more metadata for the buildpacks registry (description & keywords)
    2. Includes the wildcard stack because the Rust tools provided herein should work with any Linux

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/dmikusa/rust-dist-cnb
+module github.com/paketo-community/rust-dist
 
-go 1.14
+go 1.16
 
 require (
-	github.com/onsi/gomega v1.16.0
-	github.com/paketo-buildpacks/packit v0.14.3
+	github.com/buildpacks/libcnb v1.22.0
+	github.com/onsi/gomega v1.15.0
+	github.com/paketo-buildpacks/libpak v1.51.1
 	github.com/sclevine/spec v1.4.0
-	github.com/stretchr/testify v1.7.0
 )
--- rust/detect_test.go
+++ rust/detect_test.go
@@ -1,12 +1,10 @@
 package rust_test
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 
-	"github.com/dmikusa/rust-dist-cnb/rust"
-	"github.com/paketo-buildpacks/packit"
+	"github.com/buildpacks/libcnb"
+	"github.com/paketo-community/rust-dist/rust"
 	"github.com/sclevine/spec"
 
 	. "github.com/onsi/gomega"
@@ -16,66 +14,20 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 	var (
 		Expect = NewWithT(t).Expect
 
-		workingDir string
-		detect     packit.DetectFunc
+		ctx    libcnb.DetectContext
+		detect rust.Detect
 	)
 
-	it.Before(func() {
-		var err error
-		workingDir, err = ioutil.TempDir("", "working-dir")
-		Expect(err).NotTo(HaveOccurred())
-
-		detect = rust.Detect()
-	})
-
-	it.After(func() {
-		Expect(os.RemoveAll(workingDir)).To(Succeed())
-	})
-
-	it("returns a plan that provides rust", func() {
-		result, err := detect(packit.DetectContext{
-			WorkingDir: workingDir,
-		})
-		Expect(err).NotTo(HaveOccurred())
-		Expect(result).To(Equal(packit.DetectResult{
-			Plan: packit.BuildPlan{
-				Provides: []packit.BuildPlanProvision{
-					{Name: rust.PlanDependencyRust},
+	it("includes build plan options", func() {
+		Expect(detect.Detect(ctx)).To(Equal(libcnb.DetectResult{
+			Pass: true,
+			Plans: []libcnb.BuildPlan{
+				{
+					Provides: []libcnb.BuildPlanProvide{
+						{Name: "rust"},
+					},
 				},
 			},
 		}))
 	})
-
-	context("when BP_RUST_VERSION is set", func() {
-		it.Before(func() {
-			Expect(os.Setenv("BP_RUST_VERSION", "some-version")).To(Succeed())
-		})
-
-		it.After(func() {
-			Expect(os.Unsetenv("BP_RUST_VERSION")).To(Succeed())
-		})
-
-		it("returns a plan that provides rust and requires a specific version", func() {
-			result, err := detect(packit.DetectContext{
-				WorkingDir: workingDir,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(Equal(packit.DetectResult{
-				Plan: packit.BuildPlan{
-					Provides: []packit.BuildPlanProvision{
-						{Name: rust.PlanDependencyRust},
-					},
-					Requires: []packit.BuildPlanRequirement{
-						{
-							Name: rust.PlanDependencyRust,
-							Metadata: rust.BuildPlanMetadata{
-								VersionSource: "BP_RUST_VERSION",
-								Version:       "some-version",
-							},
-						},
-					},
-				},
-			}))
-		})
-	}, spec.Sequential())
 }
